99bc83f51231132b0b463307c9cbc5690fa89a8c
Doubrami <doubradee@gmail.com>
2022-03-10 15:28:34 -0800

    hello put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the funtion
+ */
+int main(void)
+{
+put("\"programming is like a building a multilingual puzzle");
+return (0);
+}
