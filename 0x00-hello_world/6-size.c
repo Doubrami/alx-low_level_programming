62400ae580421dc9c41768cb3463494b3e2be69e
Doubrami <doubradee@gmail.com>
2022-03-11 06:29:15 -0800

    my size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,16 @@
-#include <stdio.h>                                                                                                                                                                                                                           
+#include <stdio.h>
 /**
- *main - Entry point                                                                                                                                                                                                                         
- *Return: Always 0 (Success)                                                                                                                                                                                                                 
+ *
+ * main - Entry point
+ * 
+ *Return: Always 0 (Success)
  */
-int main(void)                                                                                                                                                                                                                               
+int main(void)
 {
-int i;                                                                                                                                                                                                                                       
-long int i2;                                                                                                                                                                                                                                 
-long long int i3;                                                                                                                                                                                                                            
-char c;                                                                                                                                                                                                                                    
-float f;                                                                                                                                                                                
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));                                                                                                                                                                              
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));                                                                                                                                                                              
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(i2));                                                                                                                                                                         
-printf(“Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(i3));     
-printf(“Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));                                                                                                                                                                             return (0);
+Printf(“Size of a char: %d byte(s)\n”, sizeof (char));
+Printf(Size of an int: %d byte(s)\n”, sizeof (int));
+Printf(Size of a long int: %byte(s)\n”, sizeof (long int));
+Printf(Size of a long long int: %byte(s)\n”, sizeof (long long int)); 
+Printf(Size of a float: %byte(s)\n”, sizeof (float));
+return (0);
 }
